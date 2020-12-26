9b7198beda1473db16370854146033c035f6d986
Nate Nelles <nnelles1@gmail.com>
2020-12-26 17:20:29 -0600

    day 5

--- src/main/scala/adventofcode2020/Day5.scala
+++ src/main/scala/adventofcode2020/Day5.scala
@@ -11,11 +11,22 @@ object Day5 {
     println(part2(lines))
   }
 
-  def part1(lines: List[String]): Int = {
-    
+  def part1(lines: List[String]): Int =
+    lines.map(convertToSeatId(_)).max
+
+
+  def convertToSeatId(seatInstructions: String): Int = {
+    val rowNumBinary = seatInstructions.substring(0, 7).toCharArray.map(ch => if (ch == 'F') '0' else '1').mkString
+    val rowNumber = Integer.parseInt(rowNumBinary, 2)
+    val seatNumBinary = seatInstructions.substring(7).toCharArray.map(ch => if (ch == 'L') '0' else '1').mkString
+    val seatNum = Integer.parseInt(seatNumBinary, 2)
+    rowNumber * 8 + seatNum
   }
 
-
-  def part2(lines: List[String]): Int = 0
+  def part2(lines: List[String]): Int = {
+    val filledSeats = lines.map(convertToSeatId(_)).toSet
+    (0 to 127 * 8 + 7).find(i => filledSeats.contains(i - 1) && !filledSeats.contains(i) && filledSeats.contains(i+1))
+      .get
+  }
 }
 
